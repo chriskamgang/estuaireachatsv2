23a8812a7c954f5144fd4d65c0e444cf06117475
chriskamgang <antayoujarod@gmail.com>
2026-06-23 09:50:26 +0100

    chore: separate mobile app into its own repository
    Move Flutter mobile app to dedicated repo estuaireachatsv2_mobile

--- apps/mobile/android/app/src/main/kotlin/com/estuaireachats/estuaire_achats/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.estuaireachats.estuaire_achats
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- apps/mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:estuaire_achats/main.dart';
-
-void main() {
-  testWidgets('App launches', (WidgetTester tester) async {
-    await tester.pumpWidget(const EstuaireAchatsApp());
-    expect(find.text('EstuaireAchats'), findsOneWidget);
-  });
-}
